76d7da437a61ee582268bfb715e1c18b2c1276db
Nikolaj-K <Amoogle@Amoogles-MacBook-Air.local>
2017-11-08 02:06:26 +0100

    RewardFunction

--- contract/ImusifyToken.cs
+++ contract/ImusifyToken.cs
@@ -1,4 +1,4 @@
-﻿using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework;
 using Neo.SmartContract.Framework.Services.Neo;
 using Neo.SmartContract.Framework.Services.System;
 using System;
@@ -12,13 +12,14 @@ namespace contract
         public static string Name() => "Imusify";
         public static string Symbol() => "IMU";
         public static byte Decimals() => 8;
-        private const ulong factor = 100000000; //decided by Decimals() // why is this still there?
-        private static BigInteger initialSupply = 100000000;
+        private const ulong factor = 100000000; //decided by Decimals()
+        private static BigInteger initialSupply = (BigInteger)100000000 * factor;
 
 
-        public static readonly byte[] Owner = { 159, 243, 169, 254, 13, 229, 158, 123, 147, 6, 65, 141, 170, 124, 37, 124, 23, 231, 250, 6, 122, 98, 209, 238, 78, 48, 88, 145, 14, 145, 155, 214, 2 };
+        public static readonly byte[] Owner = {2, 36, 228, 168, 201, 122, 97, 84, 88, 1, 96, 218, 66, 99, 122, 101, 163, 210, 255, 39, 160, 115, 42, 135, 68, 45, 19, 200, 44, 185, 158, 22, 146 };
 
-
+        // We want this back for NEP5
+        //
         //[DisplayName("transfer")]
         //public static event Action<byte[], byte[], BigInteger> Transferred;
 
@@ -34,13 +35,11 @@ namespace contract
 
                 return Owner;
             }
-
             else if (method == "deploy")
             {
                 Runtime.Log("Called deploy"); // tmp
                 return Deploy();
             }
-
             else if (method == "totalSupply")
             {
                 Runtime.Log("Called total Supply"); // tmp
@@ -51,25 +50,21 @@ namespace contract
 
                 return totalsupply;
             }
-
             else if (method == "name")
             {
                 Runtime.Log("name"); // tmp
                 return Name();
             }
-
             else if (method == "symbol")
             {
                 Runtime.Log("symbol"); // tmp
                 return Symbol();
             }
-
             else if (method == "decimals")
             {
                 Runtime.Log("decimals"); // tmp
                 return Decimals();
             }
-            
             else if (args.Length > 0)
             {
                 Runtime.Log("args.Length > 0"); // tmp
@@ -87,25 +82,45 @@ namespace contract
                     Runtime.Notify("account", balance_); // tmp
                     Runtime.Notify("account", balance); // tmp
 
-                    return true;
+                    return 0;
                 }
-
-                else if (method == "testverify") // tmp
+                else if (method == "reward") // tmp
                 {
-                    Runtime.Log("called testverify"); // tmp
+                    Runtime.Log("reward"); // tmp
+                    return Reward(account);
+                }
+                else if (method == "testverify1") // tmp
+                {
+                    Runtime.Log("called testverify1"); // tmp
 
                     if (VerifySignature(Owner, account))
                     {
-                        Runtime.Notify("worked", account);
-                        return true;
+                        Runtime.Notify("VerifySignature worked", account);
+                        return 2;
                     }
                     else // tmp
                     {
-                        Runtime.Notify("didn't work", account);
-                        return true;
+                        Runtime.Notify("NOT VerifySignature", account);
+                        return 3;
                     }
                 }
+                else if (method == "testverify2") // tmp
+                {
+                    Runtime.Log("called testverify2"); // tmp
 
+                    bool isCaller = Runtime.CheckWitness(Owner);
+
+                    if (isCaller)
+                    {
+                        Runtime.Notify("CheckWitness worked", account);
+                        return 4;
+                    }
+                    else // tmp
+                    {
+                        Runtime.Notify("NOT isCaller", account);
+                        return 5;
+                    }
+                }
                 else if (method == "transfer" && args.Length == 3)
                 {
                     Runtime.Log("called method == transfer WITH args.Length == 3"); // tmp
@@ -119,6 +134,7 @@ namespace contract
                     if (isCaller)
                     {
                         byte[] receiver = (byte[])args[1];
+
                         BigInteger amount = BytesToInt((byte[])args[2]);
 
                         Runtime.Notify("amount", amount); // tmp
@@ -127,21 +143,81 @@ namespace contract
                     }
                     else
                     {
-                        Runtime.Log("NOT method == transfer && args.Length == 3"); // tmp
-                        return false;
+                        Runtime.Log("NOT isCaller"); // tmp
+                        return 6;
                     }
                 }
-
                 else
-                    return false;  
-            }
+                {
+                    Runtime.Log("NOT method == transfer && args.Length == 3"); // tmp
 
+                    return 7;
+                }
+            }
             else
-                return false;
+            {
+                Runtime.Notify("not a suitable method for args.Length > 0", method); // tmp
+
+                return 8;
+            }
         }
 
+        private static int Reward(byte[] receiver)
+        {
+            Runtime.Log("called Reward"); // tmp
 
-        private static bool Transfer(byte[] origin, byte[] receiver, BigInteger amount)
+            bool isCaller = Runtime.CheckWitness(Owner);
+
+            if (isCaller)
+            {
+                Runtime.Notify("CheckWitness Reward worked", receiver);
+
+                int new_count = GetCount() + 1;
+         
+                BigInteger amount = RewardFunction(new_count);
+
+                Storage.Put(Storage.CurrentContext, "count", IntToBytes(new_count));
+
+                Runtime.Notify("amount", amount);
+
+                Transfer(Owner, receiver, amount);
+
+                Runtime.Notify("Transfer Reward worked", receiver);
+
+                return 9;
+            }
+            else // tmp
+            {
+                Runtime.Notify("didn't work", receiver);
+
+                return 10;
+            }
+        }
+
+        private static BigInteger RewardFunction(int n)
+        {
+            int x = 3 * n;
+
+            Runtime.Notify("RewardFunction x", x); // tmp
+
+            BigInteger y = (BigInteger)x;
+
+            return y;
+        }
+
+        private static int GetCount()
+        {
+
+            byte[] count_ = Storage.Get(Storage.CurrentContext, "count");
+
+            BigInteger count = BytesToInt(count_);
+
+            Runtime.Notify("count", count);
+
+            return (int)count;
+        }
+
+        private static int Transfer(byte[] origin, byte[] receiver, BigInteger amount)
         {
             // NOTE: The ICO template tranfer fuction is more elaborate - take over the relevant if-clauses
 
@@ -155,29 +231,29 @@ namespace contract
 
                 Runtime.Notify("account", nOriginValue); // tmp
                 Runtime.Notify("account", nTargetValue); // tmp
-                
+
                 if (nOriginValue >= 0)
                 {
-                    Storage.Put(Storage.CurrentContext, origin,  IntToBytes(nOriginValue));
+                    Storage.Put(Storage.CurrentContext, origin, IntToBytes(nOriginValue));
                     Storage.Put(Storage.CurrentContext, receiver, IntToBytes(nTargetValue));
 
                     Runtime.Notify("Transfer Successful", origin, receiver, amount, Blockchain.GetHeight());
 
                     //Transferred(origin, receiver, amount);
 
-                    return true;
+                    return 11;
                 }
             }
 
-            return false;
+            return 12;
         }
 
 
-        public static bool Deploy()
+        public static int Deploy()
         {
             // NOTE: this can be deployed over and over again. To prevent this, do
-                // byte[] total_supply = Storage.Get(Storage.CurrentContext, "totalSupply");
-                // if (total_supply.Length != 0) return false;
+            // byte[] total_supply = Storage.Get(Storage.CurrentContext, "totalSupply");
+            // if (total_supply.Length != 0) return false;
 
             Storage.Put(Storage.CurrentContext, Owner, IntToBytes(initialSupply));
             Storage.Put(Storage.CurrentContext, "totalSupply", IntToBytes(initialSupply));
@@ -188,7 +264,7 @@ namespace contract
 
             //Transferred(null, Owner, initialSupply);
 
-            return true;
+            return 13;
         }
 
 
